0392cd79e83ea7faad668ab0ee8a991c0e13fcd2
tcreusc <toncc3d@gmail.com>
2021-11-23 01:02:34 +0100

    non-linear C matrix for fmincon

--- code/PostProcessor.m
+++ code/PostProcessor.m
@@ -8,6 +8,7 @@ classdef PostProcessor < handle
         dim
         FEM
         visualizer, plottingMode
+        constraints
     end
     
     methods (Access = public)
@@ -26,8 +27,8 @@ classdef PostProcessor < handle
             obj.FEM  = cParams.FEM;
             obj.plottingMode = cParams.plottingMode;
             obj.initVisualizer();
-            buckle = obj.computeBuckling();
-            yield  = obj.computeYield();
+            obj.setConstraints();
+            obj.assembleNonLinearMatrix();
             mass   = obj.calculateMass();
         end
         
@@ -88,7 +89,55 @@ classdef PostProcessor < handle
                 end
             end
         end
-    end
+        
+        function setConstraints(obj)
+            obj.constraints.displacement.max =  25;
+            obj.constraints.displacement.min = -25;
+            obj.constraints.stress.yield = 248.2; % Yield stress (MPa)
+        end
 
+        function C = assembleNonLinearMatrix(obj)
+            dispmaxMat = obj.assembleMaxDisplacement();
+            dispminMat = obj.assembleMinDisplacement();
+            yieldMat   = obj.assembleYieldConstraint();
+            buckleMat  = obj.assembleBucklingConstraint();
+
+            C = [dispmaxMat; dispminMat; yieldMat; buckleMat];
+        end
+        
+        function mat = assembleMaxDisplacement(obj)
+            actual  = obj.FEM.displacement;
+            maxDisp = obj.constraints.displacement.max;
+            mat = actual - maxDisp;
+        end
+        
+        function mat = assembleMinDisplacement(obj)
+            actual  = obj.FEM.displacement;
+            minDisp = obj.constraints.displacement.min;
+            mat = minDisp - actual;
+        end
+
+        function mat = assembleYieldConstraint(obj)
+            actual = obj.FEM.displacement;
+            yield = obj.constraints.stress.yield;
+            mat = actual - yield;
+        end
+
+        function mat = assembleBucklingConstraint(obj)
+            stress_cr = obj.FEM.mesh.barsMatrix(:,12);
+            stress = obj.FEM.stress;
+            nel = size(stress_cr,1);
+            mat = zeros(nel,1);
+            for iElem = 1:nel
+                sig = stress(iElem,1);
+                sig_cr = stress_cr(iElem,1);
+                diff = sig_cr - abs(sig);
+                if (sig < 0 && diff < 0)
+                    mat(iElem,1) = diff;
+                end
+            end
+        end
+
+    end
     
 end
--- code/main.m
+++ code/main.m
@@ -3,21 +3,11 @@
 
 clc; clear; close all;
 
-% file1 = 'dades_stiffness.m';
-% test = TestSolver().create('STIFFNESS', file1);
-% test.checkPassed(file1)
-
-% file2 = 'dades.m';
-% test2 = TestSolver().create('FEM', file2);
-% test2.checkPassed(file2)
-
-% z = [zeros(2,1); 0.8*ones(5,1)];
-% S = [ones(11,1); 2*ones(6,1)];
-
 file1 = 'fixedData.m';
 file2 = 'variableInitialData.m';
 opt   = 1;
 [displ, stress] = calculateResults(file1,file2,opt, 'STRESS');
+% que retorni M i 
 
 function [displ, stress] = calculateResults(file1,file2,opt, plottingMode)
     pre = PreProcessor(file1);
