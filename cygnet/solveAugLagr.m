47538955afee071642e6c8667fc76c1c3feaf657
arnaupena <a.penasapena@gmail.com>
2021-12-06 12:30:53 +0100

    No tira massa

--- cygnet/solveAugLagr.m
+++ cygnet/solveAugLagr.m
@@ -3,17 +3,19 @@
 function solveAugLagr
     [sec]   = computeSectionData;
     tol     = 1e-3;
-    l_u     = 1;
-    l_sig   = 1;
+    l_u     = 30;
+    l_sig   = 30;
     rho_u   = 1;
     rho_sig = 1;
-    s0      = rand(1,345);
+    s0      = 0.99*ones(1,345);
     dS      = 5;
     [c,c_u,c_sig,La0]    = computeParameters(s0,l_u,l_sig,rho_u,rho_sig);
     [dc,dc_u,dc_sig,dLa] = computeParametersGradient(s0,sec,l_u,l_sig,rho_u,rho_sig,c_u,c_sig);
     iter = 0;
     while dS > tol || c_u > 0 || c_sig > 0
         disp(num2str(c));
+        disp(num2str(c_u));
+        disp(num2str(c_sig));
         if iter == 0
             tau = norm(s0)/norm(dLa);
         else
@@ -26,11 +28,14 @@ function solveAugLagr
                 s(isec)   = max(0,min(1,smean(isec)));
             end
             [c,c_u,c_sig,La] = computeParameters(s,l_u,l_sig,rho_u,rho_sig);
-            if norm(La) < norm(La0)
+            if La < La0
                 rightStepLength = 1;
             else
 %                 La0 = La;
                 tau = tau/2;
+                if tau < 1e-20
+                    rightStepLength = 1;
+                end
             end
         end
         l_u0   = l_u;
@@ -50,8 +55,8 @@ function [c,c_u,c_sig,La] = computeParameters(s,l_u,l_sig,rho_u,rho_sig)
     x = round(x);
     [w,v1,v2] = ISCSO_2021(x,0);
     c     = w;
-    c_u   = v1;
-    c_sig = v2;
+    c_sig = v1;
+    c_u   = v2;
     La    = c + l_u*c_u + 0.5*rho_u*c_u^2 + l_sig*c_sig + 0.5*rho_sig*c_sig^2;        
 end
 
