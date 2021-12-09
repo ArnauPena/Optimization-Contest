b99016d1244fa6c177f4f17834fa9e2412372ec7
arnaupena <a.penasapena@gmail.com>
2021-12-09 19:20:29 +0100

    FiniteDIfferences

--- cygnet/solveAugLagr.m
+++ cygnet/solveAugLagr.m
@@ -1,6 +1,9 @@
 %% Augmented Langrangian method %%
 
 function solveAugLagr
+clear
+clc
+close all
     [sec]   = computeSectionData;
     method = 'finite differences';
     run("auglagrData.m");
@@ -15,23 +18,25 @@ function solveAugLagr
     dU = 0;
     dSig = 0;
     update = 1;
-    [~,~,~,dLa,dC,dU,dSig]         = computeParametersGradient(s0,s_ini,sec,l_u,l_sig,rho_u,rho_sig,c_u,c_sig,method,update,dC,dU,dSig);
+    [dc,dc_u,dc_sig,dLa,dC,dU,dSig]         = computeParametersGradient(s0,s_ini,sec,l_u,l_sig,rho_u,rho_sig,c_u,c_sig,method,update,dC,dU,dSig);
     [cost, const_u, const_sig, lu, lsig, tauV] = computeMonitoring(c,c_u,c_sig,l_u,l_sig,tau,w0,iter,s0,...
-            cost,const_u,const_sig,lu,lsig,tauV,monitor);
+            cost,const_u,const_sig,lu,lsig,tauV,monitor,dc, dc_u, dc_sig);
     iter = iter + 1;
+    cNew = c;
     while dS > tol || c_u > 0 || c_sig > 0
         displayInfo(iter,c,c_u,c_sig);
         multiple      = (iter/10);
         multiple      = multiple/fix(multiple);
         update        = multiple;
-        [tau, c_u, c_sig, s]   = determineStepLength(s0,l_u,l_sig,rho_u,rho_sig,tau,LaNew,dLa);
+        [tau, c_u, c_sig, s]   = determineStepLength(s0,l_u,l_sig,rho_u,rho_sig,tau,LaNew,dLa,cNew);
         [l_u, l_sig]           = updateMultipliers(l_u, l_sig, rho_u, rho_sig, c_u, c_sig);
         [c,c_u,c_sig,LaTrial]  = computeParameters(s,l_u,l_sig,rho_u,rho_sig);
-        [~,~,~,dLa,dC,dU,dSig] = computeParametersGradient(s,s_ini,sec,l_u,l_sig,rho_u,rho_sig,c_u,c_sig,method,update,dC,dU,dSig);
+        [dc,dc_u,dc_sig,dLa,dC,dU,dSig] = computeParametersGradient(s,s_ini,sec,l_u,l_sig,rho_u,rho_sig,c_u,c_sig,method,update,dC,dU,dSig);
         [cost, const_u, const_sig, lu, lsig, tauV] = computeMonitoring(c,c_u,c_sig,l_u,l_sig,tau,w0,iter,s,...
-            cost, const_u, const_sig, lu, lsig, tauV, monitor);
+            cost, const_u, const_sig, lu, lsig, tauV, monitor,dc,dc_u,dc_sig);
         dS            = norm(s-s0)/norm(s0);
         s0            = s;
+        cNew          = c;
         LaNew         = LaTrial;
         iter          = iter + 1;
     end
@@ -48,17 +53,18 @@ function displayInfo(iter,c,c_u,c_sig)
      disp(num2str(c_sig));
 end
 
-function [tau, c_u, c_sig, s] = determineStepLength(s0, l_u,l_sig,rho_u,rho_sig,tau,LaNew,dLa)
-        if tau < 1e-8
-            tau = 0.05*norm(s0)/norm(dLa);
-            else
-            tau = 10*tau;
-        end
+function [tau, c_u, c_sig, s] = determineStepLength(s0, l_u,l_sig,rho_u,rho_sig,tau,LaNew,dLa,cNew)
+%         if tau < 1e-8
+%             tau = 0.05*norm(s0)/norm(dLa);
+            tau = 1/max(abs(dLa))*4/37;
+%         else
+%             tau = 10*tau;
+%         end
         rightStepLength = 0;
         while rightStepLength == 0
             smean = s0 - tau*dLa;
             s     = max(0,min(1,smean));
-            [c,c_u,c_sig,LaTrial] = computeParameters(s,l_u,l_sig,rho_u,rho_sig);
+            [cTrial,c_u,c_sig,LaTrial] = computeParameters(s,l_u,l_sig,rho_u,rho_sig);
             if LaTrial < LaNew
                 rightStepLength = 1;
             else
@@ -167,7 +173,7 @@ function [l_u, l_sig] = updateMultipliers(l_u, l_sig, rho_u, rho_sig, c_u, c_sig
 end
 
 function [cost, const_u, const_sig, lu, lsig, tauV] = computeMonitoring(c, c_u, c_sig, l_u, l_sig, tau, w0, iter, s,...
-    cost, const_u, const_sig, lu, lsig, tauV, monitor)
+    cost, const_u, const_sig, lu, lsig, tauV, monitor,dc,dc_u,dc_sig)
         cost(iter+1)      = c;
         const_u(iter+1)   = c_u;
         const_sig(iter+1) = c_sig;
@@ -175,24 +181,39 @@ function [cost, const_u, const_sig, lu, lsig, tauV] = computeMonitoring(c, c_u, 
         lsig(iter+1)      = l_sig;
         tauV(iter+1)      = tau;
         x                 = calculateSectionID(s);
-        auglagr_monitoring(monitor, 1:(iter+1), x, cost, const_sig, const_u, lu, lsig ,tauV, w0);
+        auglagr_monitoring(monitor, 1:(iter+1), x, cost, const_sig, const_u, lu, lsig ,tauV, w0, dc,dc_u,dc_sig);
 end
 
 function [dC, dV1, dV2] = calculateFiniteGradient(s)
     x0 = calculateSectionID(s);
     [c0, v10, v20] = ISCSO_2021(x0,0);
     for i = 1:numel(s)
-        s(i) = s(i)+1/37;
-        if s(i) > 1
-            s(i) = 1;
+        s1    = s;
+        s2    = s;
+        s1(i) = s(i)+1/37;
+        s2(i) = s(i)-1/37;
+
+        if s1(i) > 1
+            s1(i) = 1;
+        elseif s2(i) < 0
+            s2(i) = 0;
         end
-        x = calculateSectionID(s);
-        [c, v1, v2] = ISCSO_2021(x,0);
-        ds = 1/37;
-        dC(i)  = (c-c0) / (ds);
-        dV1(i) = (v1-v10) / (ds);
-        dV2(i) = (v2-v20) / (ds);
-        s(i) = s(i) - ds;
+            x1 = calculateSectionID(s1);
+            x2 = calculateSectionID(s2);
+            [c1, v1_1, v2_1] = ISCSO_2021(x1,0);
+            [c2, v1_2, v2_2] = ISCSO_2021(x1,0);
+            ds = 1/37;
+            dC1(i)   = (c1-c0) / (ds);
+            dV1_1(i) = (v1_1-v10) / (ds);
+            dV2_1(i) = (v2_1-v20) / (ds);
+            dC2(i)   = (c2-c0) / (ds);
+            dV1_2(i) = (v1_2-v10) / (ds);
+            dV2_2(i) = (v2_2-v20) / (ds);
+            dC(i)    = (dC1(i) + dC2(i)) / 2;
+            dV1(i)   = (dV1_1(i) + dV1_2(i)) / 2;
+            dV2(i)   = (dV2_1(i) + dV2_2(i)) / 2;
     end
 end
+
+
 
