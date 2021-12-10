6626c3aa2c113dc9b1ca375e27414d8c202fd240
arnaupena <a.penasapena@gmail.com>
2021-12-10 20:14:02 +0100

    Alternate amb el projected Gradient

--- cygnet/AlternateDirectionsAlgorithm.m
+++ cygnet/AlternateDirectionsAlgorithm.m
@@ -6,75 +6,183 @@ classdef AlternateDirectionsAlgorithm < handle
     end
     
     methods (Access = public)
+        
+
 
         function obj = AlternateDirectionsAlgorithm()
+            
+         obj.computeProjectedGradient()
+        end
 
+    end
+
+    methods (Access = private)
+        
+        function computeProjectedGradient(obj)
             monitor = AlternateDirectionsMonitor();
             s0        = 0.1*ones(1,345);
-            s0        = rand(1,345);
-
             method = 'finite differences';
             update = 1;
-            sec    = obj.computeSectionData();
-            [dc,dc_u,dc_sig] = obj.computeParametersGradient(s0,sec,method,update);
-
-%             dc = load('dc.mat', 'dc').dc;
-%             dc_u = load('dc_uAltDir.mat', 'dc_u').dc_u;
-%             dc_sig = load('dc_sigAltDir.mat', 'dc_sig').dc_sig;
-
-            [c,c_u,c_sig] = obj.computeParameters(s0);
-            iter = 1;
-            s = s0;
-            RECORD_C = 1000000;
-            RECORD_S = [];
-            DCGLOBAL = [];
-
-            while iter < 1000
-                if (iter == 1 || mod(iter,10) == 0)
+            sec    = obj.computeSectionData();             
+           
+             dc = load('dc.mat', 'dc').dc;
+             dc_u = load('dc_uAltDir.mat', 'dc_u').dc_u;
+             dc_sig = load('dc_sigAltDir.mat', 'dc_sig').dc_sig;
+             
+             obj.dCApprox = dc;
+             obj.dSigApprox = dc_sig;
+             obj.dUApprox = dc_u;
+             obj.sApprox = s0;
+             
+             s = obj.makeFeasible(s0, dc_u,dc_sig);            
+             [c,c_u,c_sig] = obj.computeParameters(s);
+             iter = 1;
+             while iter < 10000
+                 
+               if (iter == 0 || mod(iter,100) == 0)
                     update = 1;
                     disp(iter)
                     disp('fem update')
                 else
                     update = 0;
                 end
-                [~,dc_u,dc_sig] = obj.computeParametersGradient(s,sec,method,update);
-                if (c_u ~= 0 && c_sig ~=0)
-                    s = obj.makeFeasible(s, dc_u,dc_sig);
-                    [c,c_u,c_sig] = obj.computeParameters(s);
-                    objective(iter) = c;
-                    stressvio(iter) = c_sig;
-                    dispvio(iter) = c_u;
+                 
+                 
+                 [dc,dc_u,dc_sig] = obj.computeParametersGradient(s,sec,method,update);   
+
+                 h =  1/max(dc)*3/37;
+                 tau = h;
+                 hasCostDescend = false;
+                 i = 1;
+                 while ~hasCostDescend
+                     smean = s - tau*abs(dc);                
+                     sT = max(0,min(1,smean));
+                     [cNew,c_u,c_sig] = obj.computeParameters(sT);
+                     if (c_u > 0 || c_sig >0)
+                      [~,dc_u,dc_sig] = obj.computeParametersGradient(sT,sec,method,1);   
+                      sT = obj.makeFeasible(sT, dc_u,dc_sig); 
+                      [cNew,c_u,c_sig] = obj.computeParameters(sT);                     
+                     end
+                     if cNew < 1.1*c
+                        hasCostDescend = true;
+                        if (c_u ~= 0 && c_sig ~=0)
+                          sT = obj.makeFeasible(sT, dc_u,dc_sig); 
+                        end
+                     end
+                     cP(i) = cNew;
+                     tauP(i) = tau;
+                     i = i+1;
+                     tau = tau/2;
+                     
+                 end
+                 
+                 s = sT;
+                 c = cNew;
+                 objective(iter) = c;
+                 stressvio(iter) = c_sig;
+                 dispvio(iter) = c_u;
+                 z = zeros(size(s));
+                 iter = iter+1;
+                                 sections = obj.calculateSectionID(s);
+
+                 monitor.update(1:iter-1, sections, objective, stressvio, dispvio,dc,dc_sig,dc_u,z)
+
+                 
+                 
+             end
+             
+             
+        end
+        
+        function computeAlternate(obj)
+             monitor = AlternateDirectionsMonitor();
+             s0        = 0.1*ones(1,345);
+         %   s0        = rand(1,345);
+
+            method = 'finite differences';
+            update = 1;
+            sec    = obj.computeSectionData();
+%            [dc,dc_u,dc_sig] = obj.computeParametersGradient(s0,sec,method,update);
+
+             dc = load('dc.mat', 'dc').dc;
+             dc_u = load('dc_uAltDir.mat', 'dc_u').dc_u;
+             dc_sig = load('dc_sigAltDir.mat', 'dc_sig').dc_sig;
+             
+             obj.dCApprox = dc;
+             obj.dSigApprox = dc_sig;
+             obj.dUApprox = dc_u;
+             obj.sApprox = s0;
+             
+             s = obj.makeFeasible(s0, dc_u,dc_sig);
+             %s = s0;
+             
+            [c,c_u,c_sig] = obj.computeParameters(s);
+            iter = 1;
+            RECORD_C = 1000000;
+            RECORD_S = [];
+            DCGLOBAL = [];
+            
+            
+            
+            z = zeros(size(s));
+            tau = 1;
+
+            while iter < 1000
+                
+                if (iter == 1 || mod(iter,10) == 0 || mod(iter-1,10)==0)
                     update = 1;
-                    iter = iter + 1;
+                    disp(iter)
+                    disp('fem update')
+                else
+                    update = 0;
                 end
-
-                if (c<RECORD_C && c_u == 0 && c_sig == 0)
-                    RECORD_C = c;
-                    RECORD_S = s;
-                end
-
-                [dc,~,~] = obj.computeParametersGradient(s,sec,method,0);
-                DCGLOBAL(:,iter) = dc;
-                s = obj.makeCostDecrease(s, c, dc);
+                
+                [dc,~,~] = obj.computeParametersGradient(s,sec,method,0);   
+                s = obj.makeCostDecrease(s, c,dc+z,z);
                 [c,c_u,c_sig] = obj.computeParameters(s);
                 objective(iter) = c;
                 stressvio(iter) = c_sig;
                 dispvio(iter) = c_u;
                 sections = obj.calculateSectionID(s);
                 iterations = 1:1:length(objective);
-                monitor.update(iterations, sections, objective, stressvio, dispvio)
+                monitor.update(iterations, sections, objective, stressvio, dispvio,dc,dc_sig,dc_u,z)
+                DCGLOBAL(:,iter) = dc;                
                 iter = iter + 1;
                 if (c<RECORD_C && c_u == 0 && c_sig == 0)
                     RECORD_C = c;
                     RECORD_S = s;
+                end 
+                
+                
+                
+                if (c_u ~= 0 && c_sig ~=0)
+                    [~,dc_u,dc_sig] = obj.computeParametersGradient(s,sec,method,update);              
+                    sM = s + 1/tau*z;
+                    sF = obj.makeFeasible(sM, dc_u,dc_sig);
+                    [c,c_u,c_sig] = obj.computeParameters(sF);
+                    objective(iter) = c;
+                    stressvio(iter) = c_sig;
+                    dispvio(iter) = c_u;
+                    update = 1;
+                    iter = iter + 1;
+                else
+                    sF = s;
                 end
+
+                if (c<RECORD_C && c_u == 0 && c_sig == 0)
+                    RECORD_C = c;
+                    RECORD_S = s;
+                end
+                
+                z = z + tau*(s - sF);
+                s = sF;
+               
             end
-            DCGLOBAL;
-        end
-
-    end
-
-    methods (Access = private)
+            DCGLOBAL;  
+            
+            
+            
+        end        
 
         function [dc,dc_u,dc_sig] = computeParametersGradient(obj,s,sec,method,update)
             switch method
@@ -132,27 +240,39 @@ classdef AlternateDirectionsAlgorithm < handle
         end
 
         function [s] = makeFeasible(obj, s, dc_u, dc_sig)
-            tau = 1/max( max(abs(dc_u), abs(dc_sig)) )*3/37;
+            
+                [a,b] = max(abs([-dc_u;-dc_sig]));
+                is1 = b == 1;
+                g(is1) = -dc_u(is1);
+                g(~is1) = -dc_sig(~is1);            
+            
+            h =  1/max(g)*1/37;
+            tau = h;
 
             isFeasible = false;
+            i = 1;
             while ~isFeasible
-                smean = s + tau*max(-dc_u,-dc_sig);
+
+                smean = s + tau*abs(g);                
                 s = max(0,min(1,smean));
                 [c,c_u,c_sig] = obj.computeParameters(s);
                 isFeasible = max(c_u, c_sig) <= 0;
-                tau = 2*tau;
+                cp(i) = max(c_u, c_sig);
+                t(i) = tau;
+                  i = i+1;
+                tau = 1.5*tau;
             end
         end
 
-        function [s] = makeCostDecrease(obj, s, c, dc)
+        function [s] = makeCostDecrease(obj, s, c, dc,z)
             tau = 1/max(dc)*3/37;
-
+            c = c+z*s';
             hasCostDecreased = false;
             while ~hasCostDecreased
                 smean = s - tau*dc;
                 s = max(0,min(1,smean));
                 [cNew,~,~] = obj.computeParameters(s);
-                hasCostDecreased = cNew <= c;
+                hasCostDecreased = cNew + z*s' <= c;
                 tau = tau/2;
             end
         end
