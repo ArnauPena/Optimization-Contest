d3dc0bd6f81afe72b7fbf792bb05a99f143bcbdc
tcreusc <toncc3d@gmail.com>
2022-12-07 20:30:20 +0100

    reduim cost

--- ISCSO 2022/AdjacencyTests.m
+++ ISCSO 2022/AdjacencyTests.m
@@ -10,24 +10,29 @@ for iBar = 1:nBars
     secsI(iBar) = secsI(iBar) + 1;
     [dWi, dSi, Ui] = ISCSO_2022(secsI,0);
     for jBar = 1:nBars
-        secsJ  = secs;
-        secsJ(jBar) = secsJ(jBar) + 1;
-        [dWj, dSj, Uj] = ISCSO_2022(secsJ,0);
-
-        if (iBar == jBar)
-            secsIJ = secsI;
+        if (jBar>=iBar)
+            secsJ  = secs;
+            secsJ(jBar) = secsJ(jBar) + 1;
+            [dWj, dSj, Uj] = ISCSO_2022(secsJ,0);
+    
+            if (iBar == jBar)
+                secsIJ = secsI;
+            else
+                secsIJ = secsI;
+                secsIJ(jBar) = secsIJ(jBar) + 1;
+            end
+    
+            [dWij, dSij, Uij] = ISCSO_2022(secsIJ,0);
+            incUi  = Ui-U;
+            incUj  = Uj-U;
+            incUij = Uij-U;
+            sumUij = incUi + incUj;
+            A(iBar,jBar) = (sumUij - incUij)/sumUij;
         else
-            secsIJ = secsI;
-            secsIJ(jBar) = secsIJ(jBar) + 1;
+            % ja transposarem al final
         end
-
-        [dWij, dSij, Uij] = ISCSO_2022(secsIJ,0);
-        incUi  = Ui-U;
-        incUj  = Uj-U;
-        incUij = Uij-U;
-        sumUij = incUi + incUj;
-        A(iBar,jBar) = (sumUij - incUij)/sumUij;  
     end
+    
 figure(1)
 [x1, x2] = meshgrid(sort(1:nBars),sort(1:nBars));
 scatter(x1(:), x2(:), [], A(:), 'filled');
